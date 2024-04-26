6c0e1abae50c3e0b3aff5f2edb50654132066ebe
sronnyy <contato.oliveronny@gmail.com>
2024-04-26 15:50:57 -0300

    port

---
+++ tailwind.config.mjs
@@ -0,0 +1,59 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	"./node_modules/flowbite/**/*.js"
+ 		],
+	theme: {
+		extend: {
+			container: {
+				center: true,
+				padding: {
+					sm: '1rem',
+					md: '0'
+				},
+			},
+			fontFamily: {	
+				poppins: 'Poppins',
+				albert: 'Albert Sans',
+				roboto: 'Roboto',
+			},
+			colors: {
+				primary: '#0C0C0C',
+				second: '#EB3850',
+				shape: '#3333',
+			},
+		},
+	},
+	plugins: [
+		require('flowbite/plugin'),
+		function ({ addComponents }) {
+			addComponents({
+				'.container': {
+					maxWidth: '90%',
+					'@screen sm': {
+						maxWidth: '640px',
+					},
+					'@screen md': {
+						maxWidth: '768px',
+					},
+					'@screen lg': {
+						maxWidth: '1024px',
+					},
+					'@screen xl': {
+						maxWidth: '1280px',
+					},
+				},
+
+				'.card': {
+					backgroundColor: '#3490dc',
+					borderTopRightRadius: '12px',
+					borderBottomLeftRadius: '12px',
+					
+				},
+
+			});
+		},
+
+	],
+};
+
